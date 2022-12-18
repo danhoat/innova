0d8f8531b84dd30659bd453b8e3f532fc3e50f21
unknown <danhoat@gmail.com>
2022-12-18 21:54:45 +0700

    update code

--- html.php
+++ html.php
@@ -16,55 +16,49 @@ function a_new_col(){
   <?php }
 
   function a_tab_project($id = "collapse1-ph", $show = ""){
+
+
+    $args = array(
+                        'post_type' => 'du-an',
+                        'post_status' => 'publish',
+                    );
+
+    $the_query = new WP_Query($args);
+
     $img = 'https://dulichhoangnguyen.com/upload/images/mua%20xuan%20NZ.jpg';
-     $img1 = "https://static1.cafeland.vn/cafelandnew/hinh-anh/2022/12/17/186/image-20221217104751-1.jpeg";
-     $img2 = "https://anvietland.vn/wp-content/uploads/2021/05/202010-helios-quy-hoach-phan-khu-1024x593.jpg";
-     $img3 = "https://img.dothi.net/2021/12/17/Z9BcC3fq/du-an-khu-do-thi-77c8.jpg";
-     $img = INNOVA_IMG_URL.'/HasThumb.jpg';
+    $img1 = "https://static1.cafeland.vn/cafelandnew/hinh-anh/2022/12/17/186/image-20221217104751-1.jpeg";
+    $img2 = "https://anvietland.vn/wp-content/uploads/2021/05/202010-helios-quy-hoach-phan-khu-1024x593.jpg";
+    $img3 = "https://img.dothi.net/2021/12/17/Z9BcC3fq/du-an-khu-do-thi-77c8.jpg";
+    $img = INNOVA_IMG_URL.'/HasThumb.jpg';
      ?>
    <div id="<?php echo $id;?>" class="collapse <?php echo $show;?> " role="tabpanel" aria-labelledby="headingOne" data-parent="#accordion-ph">
-    <div class="card-body slick-initialized slick-slider"><div aria-live="polite" class="slick-list draggable"><div class="slick-track" role="listbox" style="opacity: 1; width: 1200px; transform: translate3d(0px, 0px, 0px);"><div class="inner slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide40" style="width: 1200px;">
+        <div class="card-body slick-initialized slick-slider"><div aria-live="polite" class="slick-list draggable"><div class="slick-track" role="listbox" style="opacity: 1; width: 1200px; transform: translate3d(0px, 0px, 0px);">
+        <div class="inner slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide40" style="width: 1200px;">
+
+            <?php
+
+            while($the_query->have_posts() ){
+                $the_query->the_post(); ?>
+
                 <article class="item-01">
                     <div class="khungAnh">
-                         <a class="khungAnhCrop" href="http://mhdi10.vn/du-an-khu-nha-o-gia-dinh-quan-doi-tai-khu-dtm-nam-ho-linh-dam.htm" tabindex="0">
-                            <img alt="Dự án Khu nhà ở gia đình quân đội tại khu ĐTM Nam hồ Linh Đàm" class="tall" src="<?php echo $img; ?>">
+                         <a class="khungAnhCrop" href="<?php the_permalink();?>" tabindex="0">
+                           <?php innova_post_thumbnail();?>
                         </a>
                     </div>
                     <h3>
-                          <a href="http://mhdi10.vn/du-an-khu-nha-o-gia-dinh-quan-doi-tai-khu-dtm-nam-ho-linh-dam.htm" title="Dự án Khu nhà ở gia đình quân đội tại khu ĐTM Nam hồ Linh Đàm" tabindex="0">Dự án Khu nhà ở gia đình quân đội tại khu ĐTM Nam hồ Linh Đàm</a>
+                          <a href="<?php the_permalink();?>" title="<?php the_title();?>" tabindex="0"><?php the_title();?></a>
                     </h3>
                 </article>
-                <article class="item-01">
-                    <div class="khungAnh">
-                         <a class="khungAnhCrop" href="http://mhdi10.vn/du-an-cai-tao-khu-tap-the-quan-doi-bac-nghia-tan-ha-noi.htm" tabindex="0">
-                     <img alt="Dự án cải tạo khu tập thể quân đội Bắc Nghĩa Tân, Hà Nội" class="wide" src="<?php echo $img;?>">
-                        </a>
-                    </div>
-                    <h3>
-                          <a href="http://mhdi10.vn/du-an-cai-tao-khu-tap-the-quan-doi-bac-nghia-tan-ha-noi.htm" title="Dự án cải tạo khu tập thể quân đội Bắc Nghĩa Tân, Hà Nội" tabindex="0">Dự án cải tạo khu tập thể quân đội Bắc Nghĩa Tân, Hà Nội</a>
-                    </h3>
-                </article>
-                <article class="item-01">
-                    <div class="khungAnh">
-                         <a class="khungAnhCrop" href="http://mhdi10.vn/du-an-khu-dan-cu-va-dich-vu-thuong-mai-da-chuc-nang-an-phu-thua-thien-hue.htm" tabindex="0">
-                     <img alt="Dự án Khu dân cư và dịch vụ thương mại đa chức năng An Phú, Thừa Thiên Huế" class="wide" src="http://mhdi10.vn/pic/Customer/downloa_636519657050685075_HasThumb_Thumb.jpg">
-                        </a>
-                    </div>
-                    <h3>
-                          <a href="http://mhdi10.vn/du-an-khu-dan-cu-va-dich-vu-thuong-mai-da-chuc-nang-an-phu-thua-thien-hue.htm" title="Dự án Khu dân cư và dịch vụ thương mại đa chức năng An Phú, Thừa Thiên Huế" tabindex="0">Dự án Khu dân cư và dịch vụ thương mại đa chức năng An Phú, Thừa Thiên Huế</a>
-                    </h3>
-                </article>
-                <article class="item-01">
-                    <div class="khungAnh">
-                         <a class="khungAnhCrop" href="http://mhdi10.vn/du-an-cong-trinh-hon-hop-co-quan-van-phong-dich-vu-thuong-mai-tai-quan-cau-giay-ha-noi.htm" tabindex="0">
-            <img alt="Dự án Công trình hỗn hợp Cơ quan - Văn phòng - Dịch vụ thương mại tại quận Cầu Giấy, Hà Nội" class="tall" src="http://mhdi10.vn/pic/Customer/CT4-Phoi-_636519655179358041_HasThumb.jpg">
-                        </a>
-                    </div>
-                    <h3>
-                          <a href="http://mhdi10.vn/du-an-cong-trinh-hon-hop-co-quan-van-phong-dich-vu-thuong-mai-tai-quan-cau-giay-ha-noi.htm" title="Dự án Công trình hỗn hợp Cơ quan - Văn phòng - Dịch vụ thương mại tại quận Cầu Giấy, Hà Nội" tabindex="0">Dự án Công trình hỗn hợp Cơ quan - Văn phòng - Dịch vụ thương mại tại quận Cầu Giấy, Hà Nội</a>
-                    </h3>
-                </article> </div></div></div></div>
+                <?php
+            }
+            ?>
+
             </div>
+             </div>
+         </div>
+     </div>
+</div>
 <?php }
 
 function du_an_html($term){
--- templates/project-highlight.php
+++ templates/project-highlight.php
@@ -13,6 +13,9 @@
  <a class="" data-toggle="collapse" href="#collapse1-ph" aria-expanded="true" aria-controls="collapse1-ph" style="" aria-hidden="true" tabindex="-1" role="option" aria-describedby="slick-slide60">Đang triển khai</a><a data-toggle="collapse" href="#collapse2-ph" aria-expanded="true" aria-controls="collapse2-ph" class="slick-current slick-active active" style="" aria-hidden="false" tabindex="-1" role="option" aria-describedby="slick-slide61">Đã thực hiện </a><a data-toggle="collapse" href="#collapse3-ph" aria-expanded="true" aria-controls="collapse3-ph" class="" style="" aria-hidden="true" tabindex="-1" role="option" aria-describedby="slick-slide62">Chuẩn bị đầu tư </a></div>
             <div class="body" id="accordion-ph" role="tablist">
                 <div class="card">
+
+
+
                     <?php  a_tab_project($id= "collapse1-ph", "show");?>
                     <?php  a_tab_project($id="collapse2-ph");?>
                      <?php a_tab_project($id="collapse3-ph");?>
