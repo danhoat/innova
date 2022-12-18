ead8ea245393d0dbc6ad7d84f21e4e02aa21f655
unknown <danhoat@gmail.com>
2022-12-18 18:16:33 +0700

    udpate css

--- functions.php
+++ functions.php
@@ -27,6 +27,7 @@ Class Inova_Theme{
     }
     function init_theme(){
         $this->theme_support();
+        $this->setup_post_type();
     }
     function innove_setup_theme(){
 
@@ -44,6 +45,54 @@ Class Inova_Theme{
             'width'  => 720,
         ) );
     }
+    function setup_post_type(){
+
+        // Set UI labels for Custom Post Type
+        $labels = array(
+            'name'                => _x( 'Dự Án', 'Post Type General Name', 'twentythirteen' ),
+            'singular_name'       => _x( 'Dự Án', 'Post Type Singular Name', 'twentythirteen' ),
+            'menu_name'           => __( 'Dự Án', 'twentythirteen' ),
+            'parent_item_colon'   => __( 'Parent Movie', 'twentythirteen' ),
+            'all_items'           => __( 'Tất Cả Dự Án', 'twentythirteen' ),
+            'view_item'           => __( 'View Dự Án', 'twentythirteen' ),
+            'add_new_item'        => __( 'Add New Movie', 'twentythirteen' ),
+            'add_new'             => __( 'Add New', 'twentythirteen' ),
+            'edit_item'           => __( 'Edit Movie', 'twentythirteen' ),
+            'update_item'         => __( 'Update Movie', 'twentythirteen' ),
+            'search_items'        => __( 'Search Movie', 'twentythirteen' ),
+            'not_found'           => __( 'Not Found', 'twentythirteen' ),
+            'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
+        );
+
+    // Set other options for Custom Post Type
+
+        $args = array(
+            'label'               => __( 'Dự Án', 'twentythirteen' ),
+            'description'         => __( 'Movie news and reviews', 'twentythirteen' ),
+            'labels'              => $labels,
+            'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+            'hierarchical'        => false,
+            'public'              => true,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_nav_menus'   => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 5,
+            'can_export'          => true,
+            'has_archive'         => true,
+            'exclude_from_search' => false,
+            'publicly_queryable'  => true,
+            //'capability_type'     => 'page',
+            'show_in_rest'        => true,
+
+            // This is where we add taxonomies to our CPT
+            'taxonomies'          => array( 'category' ),
+        );
+
+    // Registering your Custom Post Type
+    register_post_type( 'du-an', $args );
+}
+
 }
 
 
