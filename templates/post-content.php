806050ca30fef32944f809b08e154b001cda2350
unknown <danhoat@gmail.com>
2022-12-19 01:29:46 +0700

    update single

--- templates/post-content.php
+++ templates/post-content.php
@@ -1,10 +1,7 @@
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				<h1 class="post-title"><a href="<?php the_permalink();?>"> <?php the_title();?> </a> </h1>
-
 				<div class="entry-content">
 					<?php the_content(); ?>
 				</div><!-- .entry-content -->
