ec52c123d4027f6a037e08b3d27dc8008312ef8e
unknown <danhoat@gmail.com>
2022-12-19 17:23:01 +0700

    get value from db

--- templates/about-us.php
+++ templates/about-us.php
@@ -17,17 +17,7 @@ $categories = get_terms('danh-muc', array(
     <div class="text-center top-section-1">
         <strong>Chào mừng đến với</strong>
         <h2><?php echo $head_line;?></h2>
-        <div style="text-align: center;">
-           <?php echo $about_us;?></div>
-        <div style="text-align: center;">
-            &nbsp;</div>
-        <br>
-        <div>
-            <p style="margin: 0px 0px 10px; padding: 0px; color: rgb(51, 51, 51); font-family: &quot;Open Sans&quot;, sans-serif; font-size: 14px;">
-                &nbsp;</p>
-        </div>
-        <br>
-
+        <div style="text-align: center;"> <?php echo $about_us;?></div>
     </div>
 
     <ul class="inner">
