23571277b2c43a78fdc7fbf5d8aab7c8e178cdbb
unknown <danhoat@gmail.com>
2022-12-19 07:30:12 +0700

    update css

---
+++ page-linh-vuc-hoat-dong.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: linh-vuc-hoat-dong
+ */
+
+?>
+<?php get_header(); ?>
+
+
+<?php get_template_part('templates/breadcrumb');?>
+
+<main role="main" class="archive" >
+
+    <?php
+    wp_reset_query();
+
+    wp_reset_query();
+    ?>
+    <?php  get_template_part('templates/about','us'); ?>
+</main>
+
+<?php get_footer(); ?>
