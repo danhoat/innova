730bf009f615e25a7da4877da43818eb0baea190
unknown <danhoat@gmail.com>
2022-12-21 09:09:29 +0700

    udpate css

--- includes/theme.php
+++ includes/theme.php
@@ -1,15 +1,11 @@
 <?php
 
 function innova_post_thumbnail(){
-	if( has_post_thumbnail() ){
-		the_post_thumbnail();
-	} else {
-		echo '<img src="'.INNOVA_IMG_URL.'/thumbnail.jpg">';
-	}
+	echo get_innova_post_thumbnail();
 
 }
 function get_innova_post_thumbnail(){
-	return has_post_thumbnail() ? get_the_post_thumbnail() : '<img src="'.INNOVA_IMG_URL.'/thumbnail.jpg">';
+	return has_post_thumbnail() ? get_the_post_thumbnail() : '<img src="'.INNOVA_IMG_URL.'/thumbnail.png">';
 }
 function innova_custom_logo(){
 	$custom_logo_id = get_theme_mod( 'custom_logo' );
--- templates/nav.php
+++ templates/nav.php
@@ -54,7 +54,7 @@ if($style){
               <a class="nav-link disabled">Disabled</a>
             </li>
           </ul> -->
-          <form class="d-flex" role="search" action="<?php echo home_url();?>">
+          <form class="d-flex search-form" role="search" action="<?php echo home_url();?>">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="s">
             <button class="btn btn-outline-success" type="submit">Search</button>
           </form>
@@ -100,7 +100,7 @@ if($style){
         </ul>
         <form class="d-flex mt-3 search-form" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-success" type="submit">Search</button>
+          <button class="btn btn-success" type="submit">Search</button> 
         </form>
       </div>
     </div>
